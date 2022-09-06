9face0736e11efd7b20d42b8626b259289f9f0e9
wangshijun <wangshijun2010@gmail.com>
2022-09-06 12:31:06 +0800

    fix: multi test case

--- packages/multi/test/integration/writer.js
+++ packages/multi/test/integration/writer.js
@@ -3,12 +3,13 @@ const { createDataStore } = require('../../lib/index');
 const DataStore = createDataStore(Number(process.env.NEDB_MULTI_PORT));
 
 const db = new DataStore({ filename: 'test.data' });
-db.persistence.setAutoCompactionInterval(500);
 
 db.loadDatabase((err) => {
   if (err) {
     console.error(err);
   }
+
+  db.persistence.setAutoCompactionInterval(500);
 
   function next(count) {
     if (count < Number(process.env.NEDB_MULTI_ITERATIONS)) {
