b192fd31eff3cadbc1008edf1c5962c147075fa0
wangshijun <wangshijun2010@gmail.com>
2022-12-31 09:35:24 +0800

    fix: multi server should be backward compatible

--- packages/multi/test/unit/rpc.test.js
+++ packages/multi/test/unit/rpc.test.js
@@ -7,13 +7,13 @@ const { deserialize } = require('../../lib/utils');
 test('without callback', (t) => {
   const args = [{}];
   const socket = {
-    send(filename, method, dataOnlyArgs) {
-      t.deepEqual(dataOnlyArgs.map(deserialize), args);
+    send(options, method, dataOnlyArgs) {
+      t.deepEqual(dataOnlyArgs, args);
       t.end();
     },
   };
 
-  doRpc(socket, 'file', 'method', args);
+  doRpc(socket, { file: 'file' }, 'method', args);
 });
 
 test('with callback', (t) => {
@@ -30,13 +30,13 @@ test('with callback', (t) => {
     ];
 
     const socket = {
-      send(filename, method, dataOnlyArgs, callback) {
-        st.deepEqual(dataOnlyArgs.map(deserialize), args.slice(0, 1));
+      send(options, method, dataOnlyArgs, callback) {
+        st.deepEqual(dataOnlyArgs, args.slice(0, 1));
         callback(null, socketReplyArg1);
       },
     };
 
-    doRpc(socket, 'file', 'method', args);
+    doRpc(socket, { file: 'file' }, 'method', args);
   });
 
   t.test('with error', (st) => {
@@ -51,12 +51,57 @@ test('with callback', (t) => {
       },
     ];
     const socket = {
-      send(filename, method, dataOnlyArgs, callback) {
+      send(options, method, dataOnlyArgs, callback) {
+        st.deepEqual(dataOnlyArgs, args.slice(0, 1));
+        callback(errio.stringify(new Error(errorMessage)));
+      },
+    };
+
+    doRpc(socket, { file: 'file' }, 'method', args);
+  });
+});
+
+test('with callback: serialized', (t) => {
+  t.test('with no error', (st) => {
+    const socketReplyArg1 = 'arg1';
+
+    const args = [
+      { key: 'value', regexp: /value/ },
+      (err, arg1, arg2) => {
+        st.notOk(err);
+        st.deepEqual(arg1, socketReplyArg1);
+        st.end();
+      },
+    ];
+
+    const socket = {
+      send(options, method, dataOnlyArgs, callback) {
+        st.deepEqual(dataOnlyArgs.map(deserialize), args.slice(0, 1));
+        callback(null, socketReplyArg1);
+      },
+    };
+
+    doRpc(socket, { file: 'file', serialized: true }, 'method', args);
+  });
+
+  t.test('with error', (st) => {
+    const errorMessage = 'message';
+
+    const args = [
+      { key: 'value', regexp: /value/ },
+      (err) => {
+        st.true(err instanceof Error);
+        st.equal(err.message, errorMessage);
+        st.end();
+      },
+    ];
+    const socket = {
+      send(options, method, dataOnlyArgs, callback) {
         st.deepEqual(dataOnlyArgs.map(deserialize), args.slice(0, 1));
         callback(errio.stringify(new Error(errorMessage)));
       },
     };
 
-    doRpc(socket, 'file', 'method', args);
+    doRpc(socket, { file: 'file', serialized: true }, 'method', args);
   });
 });
